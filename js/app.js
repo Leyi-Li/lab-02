4234de36f909cc2d40dd615062aa9aef1a39c74d
leyla li <leylali@qq.com>
2019-07-10 13:29:36 -0700

    working on feature 2

--- js/app.js
+++ js/app.js
@@ -2,50 +2,67 @@
 
 // function startPage() {
 //   loadPhotos();
-// //   attachListeners();
+//   attachListeners();
 // }
 
 function loadPhotos() {
   const success = photos => displayPhotos(photos);
+  const success2 = photos => addKeyWord(photos);
   const failure = error => console.error(error);
   $.get('./data/page-1.json',(photos)=>{
     if(photos.length){
       success(photos);
+      success2(photos);
     }else{
       failure({'message':'something is wrong'});
     }
   }, 'json');
 }
 
-function createPhotos(photoName,path,alt,photoKey){
+function CreatePhotos(photoName,path,alt,photoKey){
   this.photoName = photoName;
   this.path = path;
   this.alt = alt
   this.photoKey = photoKey;
-  createPhotos.photoList.push(this);
+  CreatePhotos.photoList.push(this);
 }
 
-createPhotos.photoList = [];
+CreatePhotos.photoList = [];
 
 function getPhotos(photos){
   photos.forEach((photo)=>{
-    new createPhotos(photo.title,photo.image_url,photo.description,photo.keyword);
+    new CreatePhotos(photo.title,photo.image_url,photo.description,photo.keyword);
   });
 }
 
 function displayPhotos(photos){
   getPhotos(photos);
-  console.log(createPhotos.photoList);
-  for(let i = 0; i <createPhotos.photoList.length; i++){
+  console.log(CreatePhotos.photoList);
+  for(let i = 0; i <CreatePhotos.photoList.length; i++){
     const $newPhoto = $('#photo-template').clone();
-    $newPhoto.find('h2').text(createPhotos.photoList[i].photoName);
-    $newPhoto.find('img').attr('src',createPhotos.photoList[i].path).attr('alt',createPhotos.photoList[i].alt);
-    $newPhoto.find('p').text(createPhotos.photoList[i].photoKey); 
-    $('main').append($newPhoto); 
+    $newPhoto.find('h2').text(CreatePhotos.photoList[i].photoName);
+    $newPhoto.find('img').attr('src',CreatePhotos.photoList[i].path).attr('alt',CreatePhotos.photoList[i].alt);
+    $newPhoto.find('p').text(CreatePhotos.photoList[i].photoKey); 
+    $('main').append($newPhoto);
   }
+}
+
+let listOfDrop = [];
+function addKeyWord(photos){
+  photos.forEach(photo =>{
+    const $newDrop = $('.dropSelect').clone();
+    $newDrop.text(photo.keyword);
+    listOfDrop.push($newDrop);
+    // console.log($('.dropBox'),$('.dropSelect'));
+    // $('.dropBox').append($newDrop);
+  });
+}
+
+function appendDrop(){
+  function addKeyWord(photos);
   
 }
 
 // startPage();
-loadPhotos();
-
+// loadPhotos();
+$().ready((loadPhotos));
